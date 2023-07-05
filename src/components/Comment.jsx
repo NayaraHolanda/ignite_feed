5fa6944700cb0c24e9e6ab04d7f13c619b866950
Nayara Holanda <naayaraholanda@gmail.com>
2023-07-05 16:57:27 -0300

    Add ignite feed project

---
+++ src/components/Comment.jsx
@@ -0,0 +1,39 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
+import styles from './Comment.module.css'
+import { Avatar } from './Avatar'
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <Avatar
+        hasBorder={false}
+        src="https://avatars.githubusercontent.com/u/18742498?v=4"
+      />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Nayara Holanda</strong>
+              <time title="04 de julho às 13h" dateTime="2023-07-04 13:00:00">
+                Cerca de 1h atrás
+              </time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,30 @@
+import { PencilLine } from 'phosphor-react'
+
+import styles from './Sidebar.module.css'
+import { Avatar } from './Avatar'
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1451187580459-43490279c0fa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8dGVjaHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60"
+        alt=""
+      />
+
+      <div className={styles.profile}>
+        <Avatar src="https://avatars.githubusercontent.com/u/18742498?v=4" />
+
+        <strong>Nayara Holanda</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  )
+}
